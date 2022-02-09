6877a4ec691621c61c22715f51dc38fe4bbd3add
SrirahmiLutfiahA <76051059+SrirahmiLutfiahA@users.noreply.github.com>
2022-02-09 14:31:48 +0700

    m

--- resources/views/manager/lapticketing.blade.php
+++ resources/views/manager/lapticketing.blade.php
@@ -110,7 +110,7 @@
                                             <td>Nixon</td>
                                             <td><span class="label label-pink">Selesai</span></td>
                                             <td class="actions">
-                                                <a href="/mnjlating"
+                                                <a href="/detaillaptickmanager"
                                                     class="btn btn-icon waves-effect waves-light btn-info btn-xs m-b-5">
                                                     <i class="fa fa-eye"></i> <span> Detail</span></a>
                                             </td>
